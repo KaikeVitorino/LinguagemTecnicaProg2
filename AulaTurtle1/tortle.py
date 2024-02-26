d8cabe9b94c709c561ffa6831020fb5119fb4b2b
Kaike Vitorino - Conta FACULDADE <126472228+KaikeVitorino@users.noreply.github.com>
2024-02-26 10:50:04 -0300

    Turtle um
    fazer logo olimpiadas

---
+++ AulaTurtle1/tortle.py
@@ -0,0 +1,25 @@
+import turtle as tr
+import random
+
+'''
+Quadrados = 30
+lados = 3
+angulos = 360/l
+ados
+while Quadrados > 0:
+    AnguloInicial = random.randint(30, 330)
+    tr.left(AnguloInicial)
+
+    tr.forward(100)
+    tr.left(angulos)
+
+    Quadrados = Quadrados-1
+'''
+
+tr.circle(100)
+
+tr.left(90)
+tr.circle(100)
+
+tr.left(180)
+tr.circle(100)
