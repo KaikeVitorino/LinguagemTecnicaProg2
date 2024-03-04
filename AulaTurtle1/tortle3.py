131481c6ecfdb9005d32264c87d22b6b14b22b52
Kaike Vitorino - Conta FACULDADE <126472228+KaikeVitorino@users.noreply.github.com>
2024-03-04 10:54:32 -0300

    A
    A

---
+++ AulaTurtle1/tortle3.py
@@ -0,0 +1,14 @@
+from turtle import *
+import turtle
+import turtle as tr
+import random
+
+tr.speed(20)
+penes=25
+while True:
+        turtle.forward(200)
+        turtle.left(170)
+        penes = penes - 1
+
+            
+        
